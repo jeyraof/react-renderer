10a6150a3ec0a2cdbf916b1a5ba60a0fd05bf55a
Lee Jaeyoung <jaeyoung@monodiary.net>
2014-12-26 11:47:16 +0900

    Apply method

--- src/ReactRenderer.js
+++ src/ReactRenderer.js
@@ -2,21 +2,76 @@
  * @providesModule ReactRenderer
  */
 
+var React = require('React');
+
 /**
  * @constructor
+ * @param {Object} options
+ * options = {
+ *   wrapperClass: 'string'
+ * }
  */
-function ReactRenderer() {
-  // Happy new year!
+function ReactRenderer(options) {
+  var options = options || {};
+  this.wrapperClass = options.wrapperClass || 'react-box';
+
+  this.createBox = function() {
+    var reactBox = document.createElement('div');
+    reactBox.className = this.wrapperClass;
+    return reactBox;
+  };
+
+  this.clearContainer = function(container) {
+    while (container.firstChild) {
+      container.removeChild(container.firstChild);
+    }
+  };
 }
 
-ReactRenderer.prototype.append = function() {
-  // body...
+/**
+ * Implements React.render() interface
+ * Act just like React.render(), but hooked by wrapper div.
+ * @param {domElement} element
+ * @param {domElement} container
+ * @param {function} callback
+ */
+ReactRenderer.prototype.render = function(element, container, callback) {
+  var box = this.createBox();
+  this.clearContainer(container);
+  container.appendChild(box);
+  React.render(element, box, callback);
 };
 
-ReactRenderer.prototype.prepend = function() {
-  // body...
+/**
+ * Prepend box before first react-box child of container.
+ * like jQuery.prepend()
+ * @param {domElement} element
+ * @param {domElement} container
+ * @param {function} callback
+ */
+ReactRenderer.prototype.prepend = function(element, container, callback) {
+  var box = this.createBox();
+  if (container.firstChild) {
+    container.insertBefore(box, container.firstChild);
+  } else {
+    container.appendChild(box);
+  }
+  React.render(element, box, callback);
 };
 
-ReactRenderer.prototype.insert = function() {
-  // body...
+/**
+ * Append box after last react-box child of container.
+ * like jQuery.append()
+ * @param {domElement} element
+ * @param {domElement} container
+ * @param {function} callback
+ */
+ReactRenderer.prototype.append = function(element, container, callback) {
+  var box = this.createBox();
+  container.appendChild(box);
+  React.render(element, box, callback);
 };
+
+
+// Export the singleton instance.
+module.exports = ReactRenderer;
