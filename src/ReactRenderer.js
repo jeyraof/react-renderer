d3853f2c42d5c8f9ccf30975638b7f1074b69693
Lee Jaeyoung <jaeyoung@monodiary.net>
2014-12-26 10:37:18 +0900

    Add ReactRenderer.js

---
+++ src/ReactRenderer.js
@@ -0,0 +1,22 @@
+/**
+ * @providesModule ReactRenderer
+ */
+
+/**
+ * @constructor
+ */
+function ReactRenderer() {
+  // Happy new year!
+}
+
+ReactRenderer.prototype.append = function() {
+  // body...
+};
+
+ReactRenderer.prototype.prepend = function() {
+  // body...
+};
+
+ReactRenderer.prototype.insert = function() {
+  // body...
+};
